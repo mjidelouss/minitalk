eb1c1db60071b04f9a692060fe3d27b32c1886fa
ABDELMAJID ELOUSSE <77199542+mjidelouss@users.noreply.github.com>
2021-12-14 21:28:57 +0100

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelousse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 17:00:44 by aelousse          #+#    #+#             */
+/*   Updated: 2021/12/14 20:11:18 by aelousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+void	ft_putnbr(int nb);
+int		ft_atoi(char *str);
+
+#endif
